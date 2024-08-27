68c5f2239079d4b954252bb38fd87ea3f40a247e
AliHassanL12 <liaqqahl@gmail.com>
2024-08-27 20:40:23 +0100

    Write logic to play a round
    Here we have written the playRound function so that we can play
    a singular round using the selections made by botht the user and
    the computer

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -31,16 +31,112 @@ function getComputerChoice() {
    depending on the user's choice.
 
    CREATE variable getUserChoice and GET user input.
-   IF getUserChoice equals to 'Rock' THEN
-        RETURN 'Rock'
-   IF getUserChoice equals to 'Paper' THEN
-        RETURN 'Paper'
-   IF getUserChoice equals to 'Scissors' THEN 
-        RETURN 'Scissors'
+   Return getUserChoice
 */
 
 function getHumanChoice() {
-    const getUserChoice = prompt('Make a selection: Rock, Paper or Scissors?');
-    return (getUserChoice.toLowerCase() === 'rock') ? 'Rock' :
-    (getUserChoice.toLowerCase() === 'paper') ? 'Paper' : 'Scissors';
+    return prompt('Make a selection: Rock, Paper or Scissors?');
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+/*
+ Create a function playRound that takes in a humanChoice and Computer choice as 
+ possible arguments and plays a round, incrementing the round winner's score and 
+ displaying a message to the console that declares the winner.
+
+ No GUI
+ Input for humanChoice will be taken from User, and randomised for computerChoice.
+ The desired output is the winner's score to be increased and a message to be 
+ logged to the console that declares who the winner is. 
+
+ CREATE a variable getHumanChoice and GET humanChoice 
+ CREATE a variable getComputerChoice and GET computerChoice
+
+ CREATE playRound THEN
+    OBTAIN case-insensitive Human and Computer choice
+    IF humanChoice AND computerChoice are EQUAL THEN
+        DISPLAY 'Draw'
+    IF HumanChoice  is 'Rock' THEN
+        IF computerChoice is 'Paper' THEN
+            INCREMENT computerScore
+            DISPLAY 'You Lose! Paper beats Rock'
+        ELSE 
+            INCREMENT humanScore
+            DISPLAY 'You Win! Rock beats Paper'
+    IF HumanChoice  is 'Paper' THEN
+        IF computerChoice is 'Scissors' THEN
+            INCREMENT computerScore
+            DISPLAY 'You Lose! Scissors beats Paper'
+        ELSE 
+            INCREMENT humanScore
+            DISPLAY 'You Win! Paper beats Rock'
+    IF humanChoice is 'Scissors' THEN
+        IF computerChoice is 'Rock' THEN
+            INCREMENT computerScore
+            DISPLAY 'You Lose! Rock beats Scissors'
+        ELSE 
+            INCREMENT humanScore
+            DISPLAY 'You Win! Scissors beats Paper' 
+*/
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    checkForWinner(humanChoice, computerChoice);
+    
+}
+
+function checkForWinner(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Draw, both of you chose ${humanChoice}`);
+    }
+
+    else if (humanChoice === 'rock') {
+        switch(computerChoice) {
+            case 'paper':
+                computerScore++;
+                console.log('You Lose! Paper beats Rock');
+                break;
+            case 'scissors':
+                humanScore++;
+                console.log('You Win! Rock beats Scissors');
+                break;
+        }
+    }
+
+    else if (humanChoice === 'paper') {
+        switch(computerChoice) {
+            case 'scissors':
+                computerScore++;
+                console.log('You Lose! Scissors beats Paper');
+                break;
+            case 'rock':
+                humanScore++;
+                console.log('You Win! Paper beats Rock')
+                break;
+        }
+    }
+
+    else if (humanChoice === 'scissors') {
+        switch(computerChoice) {
+            case 'rock':
+                computerScore++;
+                console.log('You Lose! Rock beats Scissors');
+                break;
+            case 'paper':
+                computerScore++;
+                console.log('You Win! Scissors beats Paper')
+        }
+    }
+
+}
+
+playRound(humanSelection, computerSelection);
