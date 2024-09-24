f8b5c6a3f1fbc790175d5e130afb2982c71309fe
AliHassanL12 <liaqqahl@gmail.com>
2024-09-24 20:18:08 +0100

    Add buttons for game UI
    Allows user to play the game through buttons rather than the
    console, which is more seamless and easier, and prevents bad
    input from the part of the user. Attaching event listeners
    allows us to listen to the user clicks to observe what choice
    of buttons they made, and then, the game is played from that
    choice.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,87 +1,12 @@
-/* I need to create a function getComputerChoice that randomly returns one of 
-   three string values: 
-   'Rock', 'Paper', 'Scissors'.
-
-   Has no GUI
-   There is no input
-   The expected Output is a random string between 'Rock', 'Paper', 'Scissors';
-
-   CREATE variable randomNumberGenerator and SET it to a random number between 1 & 3.
-   IF randomNumberGenerator is 1 THEN
-        RETURN 'Rock'
-   IF randomNumberGenerator is 2 THEN
-        RETURN 'Paper'
-   IF randomNumberGenerator is 3 THEN
-        RETURN 'Scissors'
-
-*/
-
 function getComputerChoice() {
     const randomNumberGenerator = Math.floor(Math.random() * 3) + 1; //Random Number between 1 and 3
     return (randomNumberGenerator === 1) ? 'Rock' : 
     (randomNumberGenerator === 2) ? 'Paper' : 'Scissors'; 
 }
 
-/* Create a function getHumanChoice that returns the user's choice between
-   'Rock', 'Paper', 'Scissors'.
-
-   No GUI
-   Input is from user
-   Desired output is returning one of either 'Rock', 'Paper', 'Scissors' 
-   depending on the user's choice.
-
-   CREATE variable getUserChoice and GET user input.
-   Return getUserChoice
-*/
-
 function getHumanChoice() {
     return prompt('Make a selection: Rock, Paper or Scissors?');
 }
-
-
-
-
-/*
- Create a function playRound that takes in a humanChoice and Computer choice as 
- possible arguments and plays a round, incrementing the round winner's score and 
- displaying a message to the console that declares the winner.
-
- No GUI
- Input for humanChoice will be taken from User, and randomised for computerChoice.
- The desired output is the winner's score to be increased and a message to be 
- logged to the console that declares who the winner is. 
-
- CREATE a variable getHumanChoice and GET humanChoice 
- CREATE a variable getComputerChoice and GET computerChoice
-
- CREATE playRound THEN
-    OBTAIN case-insensitive Human and Computer choice
-    IF humanChoice AND computerChoice are EQUAL THEN
-        DISPLAY 'Draw'
-    IF HumanChoice  is 'Rock' THEN
-        IF computerChoice is 'Paper' THEN
-            INCREMENT computerScore
-            DISPLAY 'You Lose! Paper beats Rock'
-        ELSE 
-            INCREMENT humanScore
-            DISPLAY 'You Win! Rock beats Paper'
-    IF HumanChoice  is 'Paper' THEN
-        IF computerChoice is 'Scissors' THEN
-            INCREMENT computerScore
-            DISPLAY 'You Lose! Scissors beats Paper'
-        ELSE 
-            INCREMENT humanScore
-            DISPLAY 'You Win! Paper beats Rock'
-    IF humanChoice is 'Scissors' THEN
-        IF computerChoice is 'Rock' THEN
-            INCREMENT computerScore
-            DISPLAY 'You Lose! Rock beats Scissors'
-        ELSE 
-            INCREMENT humanScore
-            DISPLAY 'You Win! Scissors beats Paper' 
-*/
-
-
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
@@ -132,53 +57,18 @@ function checkForWinner(humanChoice, computerChoice) {
 
 }
 
-/*
-We have to create a function called playGame, that calls playRound 5 times and then
-declares a winner at the end and keeps track of humanScore and computerScore
+const rockBtn = document.querySelector('.rockBtn');
+const paperBtn = document.querySelector('.paperBtn');
+const scissorsBtn = document.querySelector('.ScissorsBtn');
 
-Input is none
-No GUI
-Desired output is a function that calls playRound 5 times, keeps track of human and 
-computer scores and ultimately declares a winner at the end
+rockBtn.addEventListener('click', () => {
+    playRound('rock', getComputerChoice());
+})
 
-CREATE variable isGameOver and SET it to false
-WHILE isGameOver is NOT TRUE 
-    CALL playRound
-    IF computerScore or humanScore EQUAL to 5 
-        SET isGameOver to TRUE
-        DISPLAY winner
-ENDWHILE
+paperBtn.addEventListener('click', () => {
+    playRound('paper', getComputerChoice());
+})
 
-*/
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let humanSelection;
-    let computerSelection;
-    let roundResult;
-    let isGameContinued = true;
-
-    while (isGameContinued) {
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-        roundResult = playRound(humanSelection, computerSelection);
-        switch (roundResult) {
-            case 'human':
-                humanScore++;
-                break;
-            case 'computer':
-                computerScore++;
-                break;
-            default:
-                break;
-        }
-        if (computerScore === 3 || humanScore === 3) {
-            isGameContinued = false; 
-        }
-    }
-
-    (humanScore === 3) ? console.log('Player Wins!') : console.log('Computer Wins!');
-}
-
-playGame();
+scissorsBtn.addEventListener('click', () => {
+    playRound('scissors', getComputerChoice());
+})
